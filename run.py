25d55285a0286c1fcbc8e8de64448e910604adb9
dlemel <dlemel8@gmail.com>
2018-09-05 22:28:03 +0300

    - added more repos - use tag if release url return empty list - rewrote using asyncio

--- run.py
+++ run.py
@@ -1,11 +1,52 @@
-import requests
+import asyncio
+import time
 
-X = 'docker/docker-ce'
-URL = f'https://api.github.com/repos/{X}/releases'
+import aiohttp
 
-r = requests.get(URL)
-released = [x for x in r.json() if not x['prerelease']]
-latest = max(released, key=lambda x: x['published_at'])
-print(latest['name'])
-print(latest['published_at'])
-print(latest['body'])
+START = time.time()
+
+REPOS = [
+    'python/cpython',
+    # 'golang/go',
+    'docker/docker-ce',
+    'docker/compose',
+    # 'linuxkit/linuxkit',
+    # # 'ansible/ansible',
+    # 'metacloud/molecule',
+    # 'pytest-dev/pytest',
+    # # 'jenkinsci/jenkins',
+    # 'mysql/mysql-server',
+    # 'nginx/nginx',
+    # 'haproxy/haproxy',
+]
+
+RELEASES_URL_FORMAT = 'https://api.github.com/repos/{:s}/releases'
+TAGS_URL_FORMAT = 'https://api.github.com/repos/{:s}/tags'
+
+
+async def get_request(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
+
+
+async def latest_release(repo):
+    response = await get_request(RELEASES_URL_FORMAT.format(repo))
+    released = [x for x in response if not x['prerelease']]
+    if released:
+        latest = max(released, key=lambda x: x['published_at'])
+        print(latest['name'], latest['published_at'])
+        # print(latest['body'])
+    else:
+        response = await get_request(TAGS_URL_FORMAT.format(repo))
+        latest = response[0]
+        print(latest['name'])
+
+
+async def main():
+    release_futures = [latest_release(r) for r in REPOS]
+    done, _ = await asyncio.wait(release_futures)
+    assert len(done) == len(release_futures)
+    print("Process took: {:.2f} seconds".format(time.time() - START))
+
+asyncio.run(main())

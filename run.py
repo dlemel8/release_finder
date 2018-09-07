0982725fa13388484bd7b3ca2a39b21b4ce4e2aa
dlemel <dlemel8@gmail.com>
2018-09-07 18:31:45 +0300

    save result to mongodb

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import asyncio
 import time
+from datetime import datetime
 
 import aiohttp
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import ReturnDocument
 
 START = time.time()
 
@@ -30,23 +33,38 @@ async def get_request(url):
             return await response.json()
 
 
-async def latest_release(repo):
+async def upsert_information(collection, name, version, **kargs):
+    result = await collection.find_one_and_update({'name': name},
+                                                  {'$set': {'version': version, 'time': datetime.now(), **kargs}},
+                                                  upsert=True, return_document=ReturnDocument.AFTER)
+    print('result %s' % repr(result))
+
+
+async def latest_release(repo, collection):
     response = await get_request(RELEASES_URL_FORMAT.format(repo))
     released = [x for x in response if not x['prerelease']]
     if released:
         latest = max(released, key=lambda x: x['published_at'])
-        print(latest['name'], latest['published_at'])
-        # print(latest['body'])
+        await upsert_information(collection, repo, latest['name'],
+                                 published_at=latest['published_at'], body=latest['body'])
     else:
         response = await get_request(TAGS_URL_FORMAT.format(repo))
         latest = response[0]
-        print(latest['name'])
+        await upsert_information(collection, repo, latest['name'])
 
 
 async def main():
-    release_futures = [latest_release(r) for r in REPOS]
+    client = AsyncIOMotorClient()
+    db = client['mydb']
+    collection = db['repos']
+
+    release_futures = [latest_release(r, collection) for r in REPOS]
     done, _ = await asyncio.wait(release_futures)
     assert len(done) == len(release_futures)
     print("Process took: {:.2f} seconds".format(time.time() - START))
 
-asyncio.run(main())
+
+# asyncio.run(main())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+

7e9327561773b233e10a5883ea4265f6cd2fd374
dlemel <dlemel8@gmail.com>
2018-09-26 19:49:50 +0300

    improved releases service printing according to old stored version

--- latest_release.py
+++ latest_release.py
@@ -11,16 +11,22 @@ RELEASES_URL_FORMAT = 'https://api.github.com/repos/{:s}/releases'
 TAGS_URL_FORMAT = 'https://api.github.com/repos/{:s}/tags'
 
 
-async def upsert_information(collection, name, version, **kargs):
-    result = await collection.find_one_and_update({'name': name},
-                                                  {'$set': {'version': version, 'time': datetime.now(), **kargs}},
-                                                  upsert=True, return_document=ReturnDocument.AFTER)
-    print('result %s' % repr(result))
+mongo_client = AsyncIOMotorClient()
+mongo_db = mongo_client['mydb']
+mongo_collection = mongo_db['repos']
 
 
-client = AsyncIOMotorClient()
-db = client['mydb']
-collection = db['repos']
+async def upsert_information(name, version, **kargs):
+    result = await mongo_collection.find_one_and_update({'name': name},
+                                                        {'$set': {'version': version, 'time': datetime.now(), **kargs}},
+                                                        upsert=True, return_document=ReturnDocument.BEFORE)
+    print(f'repo {name} old record is {result}')
+    if not result:
+        print(f'new repo {name} to track!')
+    else:
+        stored_version = result['version']
+        if version != stored_version:
+            print(f'repo {name} was upgraded from version {stored_version} to version {version}!')
 
 
 async def handle_repo(repo):
@@ -30,14 +36,14 @@ async def handle_repo(repo):
         print(f'repo {repo} releases are {released}')
         latest = max(released, key=lambda x: x['published_at'])
         print(f'repo {repo} latest release is {latest}')
-        await upsert_information(collection, repo, latest['name'],
+        await upsert_information(repo, latest['name'],
                                  published_at=latest['published_at'], body=latest['body'])
     else:
         response = await get_request(TAGS_URL_FORMAT.format(repo))
         print(f'repo {repo} tags are {response}')
         latest = response[0]
         print(f'repo {repo} latest release is {latest}')
-        await upsert_information(collection, repo, latest['name'])
+        await upsert_information(repo, latest['name'])
 
 
 # asyncio.run(consume_queue(QUEUE_NAME, handle_repo)) # TODO - why isn't this working?

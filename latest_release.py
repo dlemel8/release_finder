e235f71566008eab5a63cafced3428c72073dbf2
dlemel <dlemel8@gmail.com>
2018-09-26 19:24:49 +0300

    updated releases service to use rabbitmq

--- latest_release.py
+++ latest_release.py
@@ -1,14 +1,12 @@
 import asyncio
-import time
 from datetime import datetime
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import ReturnDocument
 
-from common import REPOS, get_request
+from common import get_request, consume_queue
 
-START = time.time()
-
+QUEUE_NAME = 'releases'
 RELEASES_URL_FORMAT = 'https://api.github.com/repos/{:s}/releases'
 TAGS_URL_FORMAT = 'https://api.github.com/repos/{:s}/tags'
 
@@ -20,31 +18,29 @@ async def upsert_information(collection, name, version, **kargs):
     print('result %s' % repr(result))
 
 
-async def latest_release(repo, collection):
+client = AsyncIOMotorClient()
+db = client['mydb']
+collection = db['repos']
+
+
+async def handle_repo(repo):
     response = await get_request(RELEASES_URL_FORMAT.format(repo))
     released = [x for x in response if not x['prerelease']]
     if released:
+        print(f'repo {repo} releases are {released}')
         latest = max(released, key=lambda x: x['published_at'])
+        print(f'repo {repo} latest release is {latest}')
         await upsert_information(collection, repo, latest['name'],
                                  published_at=latest['published_at'], body=latest['body'])
     else:
         response = await get_request(TAGS_URL_FORMAT.format(repo))
+        print(f'repo {repo} tags are {response}')
         latest = response[0]
+        print(f'repo {repo} latest release is {latest}')
         await upsert_information(collection, repo, latest['name'])
 
 
-async def main():
-    client = AsyncIOMotorClient()
-    db = client['mydb']
-    collection = db['repos']
-
-    release_futures = [latest_release(r, collection) for r in REPOS]
-    done, _ = await asyncio.wait(release_futures)
-    assert len(done) == len(release_futures)
-    print("Process took: {:.2f} seconds".format(time.time() - START))
-
-
-# asyncio.run(main())
+# asyncio.run(consume_queue(QUEUE_NAME, handle_repo)) # TODO - why isn't this working?
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+loop.run_until_complete(consume_queue(QUEUE_NAME, handle_repo))
 loop.close()
